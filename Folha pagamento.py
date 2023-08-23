a03e12c4fcd37fa0e27cba0a68484f31a5569bd3
Jansen <89665139+Jansen-Killinger-Cara@users.noreply.github.com>
2023-08-23 19:11:30 -0300

    Folha de Pagamento Completa.
    Versão final do sistema de calculo de folha de pagamento com os descontos de INSS, IRRF, Vale Transporte e cálculo de Dependentes.

--- Folha pagamento.py
+++ Folha pagamento.py
@@ -2,82 +2,59 @@
 #Calculando o valor do salário descontando o INSS e o Vale Transporte
 
 import math
+salarioB = float(input("Digite o Salário Bruto: "))
+vale = input("Desncontar Vale Transporte? ")
+dep  = input ("Quantos dependetes possui? ")
+dep = 189.90
 
-salarioB = float(input("Digite o salário: "))
-vale =  input("Descontar o Vale Transporte? ")
-irrf = 0.00
-if salarioB <1320.01:
-    inss = salarioB*0.075
-    print (f"Calculo do INSS R${inss}")
-    salarioL = (salarioB-inss)
-    print ("Seu salário liquído é de: R$", salarioL)
-elif salarioB > 1320.00 and salarioB <2571.30:
-    inss = (salarioB - 1320)*0.09 + 99
-    print (f"Calculo do INSS R${inss}")
-    salarioL = (salarioB-inss)
-    print ("Seu salário liquído é de: R$", salarioL)
-elif salarioB > 2571.29 and salarioB < 3856.95:
-    inss = (salarioB - 2571.29) * 0.12 + 99 + 112.62
-    print (f"Calculo do INSS R${inss}")
-    salarioL = (salarioB-inss)
-    print ("Seu salário liquído é de: R$", salarioL)
-elif salarioB > 3856.94 and salarioB < 7507.50:
-    inss = (salarioB - 3856.95) * 0.14 + 99 + 112.62 + 154.28
-    print (f"Calculo do INSS R${inss}")
-    salarioL = (salarioB-inss)
-    print ("Seu salário liquído é de: R$", salarioL)
-elif salarioB >7507.49:
-    inss = (salarioB - 3856.95) * 0.14 + 99 + 112.62 + 154.28
-    print (f"Calculo do INSS R${inss}")
-    salarioL = (salarioB-inss)
-    print ("Seu salário liquído é de: R$", salarioL, " antes do IRRF")
+#INSS
+if salarioB < 1320.01:
+    inss = float(salarioB*0.075)
+    print (f"Desconto de INSS: R${inss}")
+elif salarioB < 2571.30:
+    inss = float((salarioB-1320.00)*0.09 +99.00)
+    print (f"Desconto de INSS: R${inss}")
+elif salarioB < 3856.95:
+    inss = float((salarioB-2571.29)*0.12 + 211.62)
+    print (f"Desconto de INSS: R${inss}")
+elif salarioB < 7507.49:
+    inss = float((salarioB-3856.94)*0.14 + 365.9)
+    print (f"Desconto de INSS: R${inss}")
 else:
-    print("Ops! Algo deu errado contacte seu programador! INSS")
+    salarioB > 7507.50
+    inss = float( 876.95)
+    print (f"Desconto de INSS: R${inss}")
 
-# aqui começo a calcular o IRRF
-if salarioL > 2112.00 and salarioL <2826.66:
-    irrf = (salarioL * 0.075) - 148.40
-    print (f"Calculo do IRRF R${irrf}, salario líquido: R${salarioL}")
-elif salarioL > 2826.65 and salarioL < 3751.06:
-    irrf = (salarioL * 0.15) - 370.40
-    print (f"Calculo do IRRF R${irrf}, salario líquido: R${salarioL}")
-elif salarioL > 3751.05 and salarioL < 4664.69:
-    irrf = (salarioL * 0.225)-651.73
-    print (f"Calculo do IRRF R${irrf}, salario líquido: R${salarioL}")
-elif salarioL > 4664.68:
-    irrf = (salarioL * 0.275) -884.96
-    print (f"Calculo do IRRF R${irrf}, salario líquido: R${salarioL}")
+#Vale Transporte
+if vale == "Sim" or vale == "sim":
+    Descvale = salarioB*0.06
+    print (f"Desconto do Vale Transporte: R${Descvale}")
 else:
-    print("Ops! Algo deu errado contacte seu programador! IRRF")
+     Descvale = 0
+     print (f"Desconto do Vale Transporte: R${Descvale}")
 
-print (f"Calculo do IRRF R${irrf}")
-salarioL = (salarioB-inss) - irrf
-print ("Seu salário liquído é de: R$", salarioL)
+#IRRF
+bcirrf = salarioB - inss
+bcirrf = bcirrf - dep
+if bcirrf < 2112.01:
+    irrf = float(bcirrf*0)
+    print (f"Desconto de IRRF R${irrf}")
+elif bcirrf < 2826.66:
+    irrf = float(bcirrf*0.075 - 158.40)
+    print (f"Desconto de IRRF R${irrf}")
+elif bcirrf < 3751.06:
+    irrf = float(bcirrf*0.15 - 370.40)
+    print (f"Desconto de IRRF R${irrf}")
+elif bcirrf <= 4664.68:
+    irrf = float(bcirrf*0.2250 - 651.73)
+    print (f"Desconto de IRRF R${irrf}")
+else:
+    bcirrf >= 4664.69 
+    irrf = float(bcirrf*0.275 - 876.95)
+    print (f"Desconto de IRRF R${irrf}")
 
-#    irrf = (salarioB - 3856.95) * 0.14 + 99 + 112.62 + 154.28
-#    print (f"Calculo do INSS R${inss}")
-#    salarioL = (salarioB-inss)
-#    print ("Seu salário liquído é de: R$", salarioL)
-
-if vale == "s" or vale == "S":
-    valVale = (salarioB-inss)*0.06
-    print (f"Desconto do vale transporte é de R${valVale}")
-    salarioL = (salarioB-inss) - valVale
-    print ("Seu salário liquído é de: R$", salarioL)
-    
-
-    
-    
-    
-    
-    
-    
-    
-    
-
-
-
-
-
-
+    #sALDO
+desconto = float(inss + irrf + Descvale)
+salarioL = float(salarioB - desconto)
+print (f"Seu salário líquido é de R${salarioL}")
 
